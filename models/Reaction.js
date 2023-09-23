ff59f7c0fbce032ca167566bfbd68eb008bbc5a8
Youssef <youssefkaroui6@gmail.comm>
2023-09-22 21:21:36 -0400

    defined the reactionSchema

---
+++ models/Reaction.js
@@ -0,0 +1,36 @@
+// requires moment 
+const moment = require('moment');
+// requires Mongoose
+const { Schema, Types } = require('mongoose');
+
+//  Definning the reactionSchema 
+const reactionSchema = new Schema(
+    {
+      reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId(),
+      },
+      reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280,
+      },
+      username: {
+        type: String,
+        required: true,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (timestamp) => moment(timestamp).format('MMM Do, YYYY [at] hh:mm a'),
+      },
+    },
+    {
+      toJSON: {
+        getters: true,
+      },
+      id: false,
+    }
+  );
+  
+  module.exports = reactionSchema;
