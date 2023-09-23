0c93be1253fce8d04798ffdbf9d70dcdfe44cad2
Youssef <youssefkaroui6@gmail.comm>
2023-09-23 16:34:28 -0400

    user controller almost done, need to find out if crednetials are needed to initiate the MONGODB

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -14,3 +14,108 @@ const userController = {
         res.status(500).json(err);
       });
   },
+   // Get a single user by id
+   getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .select('-__v')
+      .populate('friends')
+      .populate('thoughts')
+      .then((userData) => {
+        if (!userData) {
+          return res.status(404).json({ message: 'User not found.' });
+        }
+        res.json(userData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+
+  // Create a  user
+  createUser(req, res) {
+    User.create(req.body)
+      .then((userData) => {
+        res.json(userData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+
+  // Update a user
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      {
+        $set: req.body,
+      },
+      {
+        runValidators: true,
+        new: true,
+      }
+    )
+      .then((userData) => {
+        if (!userData) {
+          return res.status(404).json({ message: 'User not found, please try again.' });
+        }
+        res.json(userserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+
+   // delete user along with their thoughts
+   deleteUser(req, res) {
+    User.findOneAndDelete({ _id: req.params.userId })
+      .then((userData) => {
+        if (!userData) {
+          return res.status(404).json({ message: 'User not found.' });
+        }
+
+        // get user id and delete their associate thoughts
+        return Thought.deleteMany({ _id: { $in: userData.thoughts } });
+      })
+      .then(() => {
+        res.json({ message: 'User and associated thoughts deleted!' });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+
+  // Add a friend
+  addFriend(req, res) {
+    User.findOneAndUpdate({ _id: req.params.userId }, { $addToSet: { friends: req.params.friendId } }, { new: true })
+      .then((userData) => {
+        if (!userData) {
+          return res.status(404).json({ message: 'User not found.' });
+        }
+        res.json(userData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+  // Remove a friend
+  removeFriend(req, res) {
+    User.findOneAndUpdate({ _id: req.params.userId }, { $pull: { friends: req.params.friendId } }, { new: true })
+      .then((userData) => {
+        if (!userData) {
+          return res.status(404).json({ message: 'User not found.' });
+        }
+        res.json(userData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+};
+
+module.exports = userController;
